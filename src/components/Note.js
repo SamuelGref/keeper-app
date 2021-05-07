79ea4375ac8f7d00951f1d1ad724fd933e3a4b23
Samuel Gref <samgref@gmail.com>
2021-05-07 08:29:38 -0400

    keeper app part 1

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return(
+        <div className="note">
+            <h1>this is the note titel</h1>
+            <p>This is the not content</p>
+        </div>
+    );
+}
+
+export default Note; 
